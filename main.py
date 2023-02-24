83c3c7ee50f2e304ae4a333a819bffd6b6458678
Raj Kumar Phagami <magar.fagami@gmail.com>
2023-02-24 18:49:31 -0500

    created the API
    -API for the accepting the file into the folder of backend
    - API for giving access to the public to play music
    -requirements file updated

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-from typing import Union
+import io,os
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import FileResponse
 
-from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+# Mount the "music" folder as a static directory
+app.mount("/music", StaticFiles(directory="music"), name="music")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/upload-music")
+async def create_upload_file(file: UploadFile = File(...)):
+    if not os.path.exists('music'):
+        os.makedirs("music")
+        file_location = os.path.join("music", file.filename)
+        with open(file_location, "wb") as buffer:
+            buffer.write(await file.read())
+    else:
+        pass   
+    return {"filename": file.filename,"status":200}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/get-music")
+def get_music():
+    file_list = os.listdir("music")
+    files = []
+    if file_list:
+        file_urls = [{"url":f"/music/{filename}","filename":{filename}} for filename in file_list]
+        return {"file_urls": file_urls}
+    else:
+        return {"error": "No files found in the music folder."}
